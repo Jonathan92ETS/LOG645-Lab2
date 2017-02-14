c36479da7fdd79f4188479257cadb3e433d25095
Stéphane Lam <stelam@gmail.com>
2017-02-13 22:56:37 -0500

    exchange and torsion for 2nd problem

--- lab2.cpp
+++ lab2.cpp
@@ -9,7 +9,7 @@
 
 
 #define MATRIX_ROW_LENGTH 10
-#define SLEEP_TIME 50000
+#define SLEEP_TIME 5000
 
 void print_final_matrix(int *matrix, int k);
 void init_matrix(int *matrix, int matrixSize, int starting_value);
@@ -43,13 +43,17 @@ int main(int argc, char** argv) {
 		stop_timer(&timeStart);
 	}
 	else {
+		start_timer(&timeStart);
 		init_matrix(matrix, MATRIX_SIZE, starting_value);
 		second_operation_seq(matrix, K);
 		print_final_matrix(matrix, K);
+		stop_timer(&timeStart);
 
+		start_timer(&timeStart);
 		init_matrix(matrix, MATRIX_SIZE, starting_value);
-		second_operation_seq(matrix, K);
+		second_operation_par(matrix, K);
 		print_final_matrix(matrix, K);
+		stop_timer(&timeStart);
 	}
 
 
@@ -88,27 +92,6 @@ void first_operation_seq(int *matrix, int k) {
 }
 
 
-void second_operation_seq(int *matrix, int k) {
-	int current_k, current_offset, value_at_previous_k, value_at_previous_j, i, j;
-	for (current_k = 1; current_k <= k; current_k++) {
-		for (i = 0; i < MATRIX_ROW_LENGTH; i++) {
-			for (j = MATRIX_ROW_LENGTH - 1; j >= 0; j--) {
-				value_at_previous_k = matrix[get_offset(current_k - 1, i, j)];
-				current_offset = get_offset(current_k, i, j);
-
-				value_at_previous_j = matrix[get_offset(current_k, i, j + 1)];
-				usleep(SLEEP_TIME);
-				if (j == (MATRIX_ROW_LENGTH - 1)) {
-					matrix[current_offset] = value_at_previous_k;
-				} else {
-					matrix[current_offset] = value_at_previous_k + value_at_previous_j;
-				}
-				
-			}
-		}
-	}
-}
-
 void first_operation_par(int *matrix, int k) {
 	int current_k, i, j, rang, nprocs;
 	#pragma omp parallel private(current_k,i,j, rang, nprocs)
@@ -129,23 +112,52 @@ void first_operation_par(int *matrix, int k) {
 }
 
 
-void second_operation_par(int *matrix, int k) {
-/*	int rang,i,j;
-	#pragma omp parallel 
-	#pragma omp private(rang,i,j)
-	#pragma omp for
-	{
-	// 	//usleep(50000);
-		i = rang / MATRIX_ROW_LENGTH;
-		j = rang % MATRIX_ROW_LENGTH;
-		for(int current_k = 1; current_k <= k; current_k++)
-		{ 
-			usleep(SLEEP_TIME);
-			matrix[get_offset(current_k, i, j)] = matrix[get_offset(current_k - 1, i, j)] + ((i + j) * current_k);
-			
-		}
+void second_operation_seq(int *matrix, int k) {
+	int current_k, current_offset, value_at_previous_k, value_at_previous_j, i, j;
+	for (current_k = 1; current_k <= k; current_k++) {
+		for (i = 0; i < MATRIX_ROW_LENGTH; i++) {
+			for (j = MATRIX_ROW_LENGTH - 1; j >= 0; j--) {
+				current_offset = get_offset(current_k, i, j);
+				value_at_previous_k = matrix[get_offset(current_k - 1, i, j)];
+				value_at_previous_j = matrix[get_offset(current_k, i, j)];
 
-	}*/
+				usleep(SLEEP_TIME);
+				if (j == (MATRIX_ROW_LENGTH - 1)) {
+					matrix[current_offset] = value_at_previous_k + i;
+				} else {
+					matrix[current_offset] = value_at_previous_k + value_at_previous_j;
+				}
+				
+			}
+		}
+	}
+}
+
+void second_operation_par(int *matrix, int k) {
+	int current_k, current_offset, value_at_previous_k, value_at_previous_j, i, j, nprocs, rang;
+	#pragma omp parallel private(current_k, i, j, rang, nprocs, current_offset, value_at_previous_k, value_at_previous_j)
+	{
+		#pragma omp for
+		for (i = 0; i < MATRIX_ROW_LENGTH; i++) {
+			for (current_k = 1; current_k <= k; current_k++) {
+				for (j = MATRIX_ROW_LENGTH - 1 + i; j >= 0 + i; j--) {
+					current_offset = get_offset(current_k, i, j-i);
+
+					rang = omp_get_thread_num();
+					nprocs = omp_get_num_threads();					
+					// printf("Bonjour, je suis %d (parmi %d threads) pour valuer i = %d\n", rang, nprocs, i);
+					
+					usleep(SLEEP_TIME);
+					if (j-i == (MATRIX_ROW_LENGTH - 1)) {
+						matrix[current_offset] = matrix[get_offset(k-1, i, j-i)] + i;
+					} else {
+						matrix[current_offset] = matrix[get_offset(k-1, i, j-i)] + matrix[get_offset(k, i, j+1-i)];
+					}
+					
+				}
+			}
+		}
+	}
 }
 
 void init_matrix(int *matrix, int matrixSize, int startingValue) {
